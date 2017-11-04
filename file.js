80695347784fa9e8e18d24cfb243bd9ef66ff18a
WindWalk12 <nhygta@hotmail.com>
2017-11-04 23:53:15 +0100

    Update file.js

--- file.js
+++ file.js
@@ -12,10 +12,9 @@
 const url = window.location.href;
 
 //Check if they are on kissanime
-const url1 = /^http:\/\/+kissanime\.ru\/[A-Z]*/g;
-const url2 = /Episode-+[0-9]*\?+id=+[0-9]*$/g;
+const url1 = /^http:\/\/+ekstrabladet\.dk\/[A-Z]*/g;
 
-if (url1.test(url) && url2.test(url) ) {
+if (url1.test(url)) {
 	
 	//Enable Jquery
 	var script = document.createElement('script');
@@ -23,5 +22,4 @@ if (url1.test(url) && url2.test(url) ) {
 	script.type = 'text/javascript';
 	document.getElementsByTagName('head')[0].appendChild(script);
 
-	document.getElementById('adsIfrme11').remove();
-	document.getElementById('divFloatRight').remove();
+	document.getElementsByClassName('p-absolute p-absolute--fill').parent().remove();
